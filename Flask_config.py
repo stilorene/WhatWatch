08d23128a4860203fd85b6e5f23575e13cbf2a0e
Rene Marchner <rene.marchner80@gmail.com>
2025-05-07 18:32:00 +0200

    Sesssionspeicherung implementiert, das bau ich aber noch aus, sowas wie das automatische zurückgehen durch den User macht das irgendwie total kaputt, ansonsten noch die 3 Frage implementiert, aber erstmal zu Testzwecken

--- Flask_config.py
+++ Flask_config.py
@@ -3,25 +3,36 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 app = Flask(__name__)
 app.secret_key = "0890980800"
 
+questions = ["question1.html", 
+             "question2.html",
+             "question3.html"]
+
+
 @app.route("/", methods=["GET", "POST"])
 def home():
-    question_number = 1
+    if "q_idx" not in session:
+        session["q_idx"] = 1 #nötig für die Buttons
+        # return render_template("index.html", block_to_include=f"blocks_to_include/question1.html")
 
     if request.method == "POST":
-
         nav = request.form.get("navigation")
-        if nav == "next" and question_number < 3:
-            question_number += 1
 
-        elif nav == "prev" and question_number > 1:
-            question_number = -1
+        # Checkboxen auslesen
+        streamprovider = request.form.getlist("Streaming")  # getlist() gibt alle ausgewählten Werte zurück aus dem html
+
+        # Die Buttonlogik ist hier implementiert
+        if nav == "next" and session["q_idx"] < 2:
+            session["q_idx"] += 1
+        
+        elif nav == "prev" and session["q_idx"] > 1:
+            session["q_idx"] -= 1
 
 
-    block = f"blocks_to_include/question{question_number}.html"
-    return render_template("index.html", block_to_include=block, question_number=question_number)
-
-
+        # Lade die gespeicherten Interessen aus der Session
+        saved_streamprovider = session.get("interessen", [])
+        return render_template("index.html", block_to_include=f"blocks_to_include/question{session["q_idx"]}.html")
+    return render_template("index.html", block_to_include=f"blocks_to_include/question1.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
 
