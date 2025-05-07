acf722e5c0bd2bcb0112aa4fe5b5ca1b0f76ddd0
Rene Marchner <rene.marchner80@gmail.com>
2025-05-07 20:41:06 +0200

    perfect, habs mit javascript gemacht, is viel einfacher und unkomplizierter, zumindest für die Einfachen Sachen, das schein wohl auch gängiger zu sein als zu versuchen mit flask das dynamisch zu gestalten. Flask lasse ich aber mal drin, vl kann man noch mit den routes was anfangen.

---
+++ dynamic_question_loader.js
@@ -0,0 +1,28 @@
+let counter = 1;
+
+function loadHTMLContent(counter) {
+
+    
+    let filename = `question_templates/question${counter}.html`;
+
+ 
+    fetch(filename)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Netzwerkantwort war nicht ok');
+            }
+            return response.text();
+        })
+        .then(html => {
+            // Fügt den geladenen HTML-Inhalt in das Ziel-div ein
+            document.getElementById('dynamic-question').innerHTML = html;
+        });
+}
+
+loadHTMLContent(counter)
+
+// Button-Click: Counter erhöhen und neuen Content laden
+document.getElementById("nextbtn").addEventListener("click", function () {
+    counter++;
+    loadHTMLContent(counter);
+});
